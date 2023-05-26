996b8ed33330777ade2c65574443b6058a0440c2
benoit.reynaud <benoit.reynaud@laplateforme.io>
2023-05-26 12:33:18 +0200

    Fin des jobs

---
+++ jour02/job03/index.php
@@ -0,0 +1,16 @@
+<?php
+for ($i = 0; $i <= 100; $i++) {
+    if ($i >= 0 && $i <= 20) {
+        echo "<i>$i</i><br />";
+    } 
+    elseif ($i ===42){
+        echo"<i>La Plateforme_</i><br/>";
+    }
+    elseif ($i >= 25 && $i <= 50) {
+        echo "<u>$i</u><br />";
+    } 
+    else {
+        echo "$i<br />";
+    }
+}
+?>
